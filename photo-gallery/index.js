854a7bc457bd5b6490fee66cecc1facbea978833
nguyenubuntu <nguyenubuntu@gmail.com>
2023-11-05 15:36:18 +0700

    Initial commit

--- photo-gallery/index.js
+++ photo-gallery/index.js
@@ -2,7 +2,7 @@ const btnEl = document.getElementById("btn")
 
 function fetchImage(){
     const inputValue = document.getElementById("input").value;
-    fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1`).then((res)=>res.json().then((data)=>{
+    fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1&client_id=3ReQ3BKD3VWrWLGxEt0Dj4J69ZD-fNKbfa7on8JQuNA`).then((res)=>res.json().then((data)=>{
         console.log(data);
     }));
 }
